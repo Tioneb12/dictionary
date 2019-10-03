d1b4271318924c9f5d70dd67a0501e7dcd85f690
benoitzeller <benoit.zeller@gmail.com>
2019-10-03 15:53:56 +0200

    Create word model

--- db/migrate/20191003130205_create_words.rb
+++ db/migrate/20191003130205_create_words.rb
@@ -1,6 +1,12 @@
 class CreateWords < ActiveRecord::Migration[6.0]
   def change
     create_table :words do |t|
+      t.string :title
+      t.string :excerpt
+      t.string :content
+      t.string :author_name
+      t.string :author_email
+      t.boolean :actived
 
       t.timestamps
     end
